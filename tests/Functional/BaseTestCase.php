f859ebecaaee915ae6541bc213b1448484563a99
Krishnan <eswar.krishnan@p34680.intra.rakuten.co.jp>
2018-07-08 15:15:39 +0900

    change1 to BaseTestCase.php

--- tests/Functional/BaseTestCase.php
+++ tests/Functional/BaseTestCase.php
@@ -10,6 +10,7 @@ use Slim\Http\Environment;
 /**
  * This is an example class that shows how you could set up a method that
  * runs the application. Note that it doesn't cover all use-cases and is
+ * Change 1
  * tuned to the specifics of this skeleton app, so if your needs are
  * different, you'll need to change it.
  */
